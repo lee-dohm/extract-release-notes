84704dd32df0e865542f04d01a94e715ba85272e
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-06-21 08:27:58 -0700

    Make action error out when no release notes section is found

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,8 @@ fi
 jq .pull_request.body "$GITHUB_EVENT_PATH" > "$HOME/pull-request-body.md"
 
 ./split "$HOME/pull-request-body.md" "$GITHUB_WORKSPACE/$RELEASE_NOTES_PATH"
+
+if [ -z "$(cat $GITHUB_WORKSPACE/$RELEASE_NOTES_PATH)" ]; then
+  echo "Error: No release notes found"
+  exit 1
+fi
