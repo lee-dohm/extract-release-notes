08f27ff0195b7f2282fd17606675830783b03a8b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-06-21 12:41:05 -0700

    Split is in the root directory 🤦‍♂️

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ fi
 
 jq .pull_request.body "$GITHUB_EVENT_PATH" > "$HOME/pull-request-body.md"
 
-./split "$HOME/pull-request-body.md" "$GITHUB_WORKSPACE/$RELEASE_NOTES_PATH"
+/split "$HOME/pull-request-body.md" "$GITHUB_WORKSPACE/$RELEASE_NOTES_PATH"
 
 if [ -z "$(cat $GITHUB_WORKSPACE/$RELEASE_NOTES_PATH)" ]; then
   echo "Error: No release notes found"
